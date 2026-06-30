2ede8cf7690d0b2b55d4b3ea50c46350b39e7c2f
mananalisyed23 <manansoh2020@gmail.com>
2026-06-30 01:09:02 -0700

    30-06-26

--- widgets/container/lib/screens/container.dart
+++ widgets/container/lib/screens/container.dart
@@ -7,17 +7,49 @@ class ContainerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Container'), backgroundColor: Colors.purple),
-      body: Container(
-      height: 100,width: 200,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(color: Colors.black, width: 3),
-        boxShadow: [
-          
-        ]
+      body: Column(children: [
+        Container(
+        height: 100,
+        width: 200,
+        margin: EdgeInsets.fromLTRB(100, 50, 100, 100),
+        decoration: BoxDecoration(
+          //color: Colors.blue,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Colors.black, width: 3),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withAlpha(50),
+              blurRadius: 5,
+              offset: Offset(10, 10),
+            ),
+          ],
+          //shape: BoxShape.circle,
+          gradient: LinearGradient(
+            colors: [
+              Colors.tealAccent,
+              Colors.greenAccent,
+              Colors.amberAccent,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+          ),
+        ),
       ),
+      Container(
+        height: 100,
+        width: 200,
+        margin: EdgeInsets.fromLTRB(100, 50, 100, 100),
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        color: Colors.teal,
+        child: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(image: NetworkImage('https://tse1.mm.bing.net/th/id/OIP._vFhp4PlmDjOzFomMKxSwgHaE8?pid=Api&h=220&P=0'))
+        ),
+        ),
       ),
+      ],) 
     );
   }
 }
