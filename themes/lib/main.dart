47be4a87c21097ffa3c5c1456eafb2eca670a40c
mananalisyed23 <manansoh2020@gmail.com>
2026-07-09 06:00:07 -0700

    08-07-26

---
+++ themes/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:themes/screens/themes.dart';
+
+void main() {
+  runApp(Themesday());
+}
+
+class Themesday extends StatelessWidget {
+  const Themesday({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.tealAccent,
+        // for scafold-->scaffoldBackgroundColor: const Color.fromARGB(255, 165, 253, 232),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.red,
+          foregroundColor: Colors.white,
+          padding: EdgeInsets.all(15),
+        )
+      )
+      ),
+      home: ThemesScreen(),
+    );
+  }
+}
---
+++ themes/lib/screens/themes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ThemesScreen extends StatelessWidget {
+  const ThemesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 10,
+          children: [
+            ElevatedButton(onPressed: (){},
+            child: Text('Themed button')),
+            ElevatedButton(onPressed: (){},
+            child: Text('Themed button')),
+            ElevatedButton(onPressed: (){},
+            child: Text('Themed button')),
+            Text('Manan',
+            style: Theme.of(context).textTheme.displayMedium,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
