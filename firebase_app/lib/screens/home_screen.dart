3cc2c6477cd28b699cbeae8726d434c56b747e9e
mananalisyed23 <manansoh2020@gmail.com>
2026-08-03 01:05:13 -0700

    02-08-26

--- firebase_app/lib/screens/home_screen.dart
+++ firebase_app/lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_app/screens/add_user.dart';
+// ignore: unused_import
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,35 +12,50 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final dbRef = FirebaseFirestore.instance.collection('users');
+  List<Map<String, dynamic>> users = [];
+  void fetchData() async {
+    users.clear();
+    final data = await dbRef.get();
+    setState(() {
+      for (var doc in data.docs) {
+        users.add(doc.data());
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
-      body: Column(
+      body: ListView(
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 250,
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => AddUser()),
-                    );
-                  },
-                  icon: Icon(Icons.person_add),
-                  label: Text('Sign Up'),
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(vertical: 12),
-                  ),
-                ),
+          for (var user in users)
+            ListTile(
+              title: Text(user['name']), 
+              subtitle: Text(user['email']),
+              trailing: IconButton(
+              onPressed: (){
+                dbRef.doc(user['id']).delete();
+              }, 
+              icon: Icon(Icons.delete)),
               ),
-            ],
-          ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddUser()),
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
