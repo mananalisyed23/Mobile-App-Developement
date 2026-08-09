e0c118a3d4afb834a84d6b22ae0b10ca5b6c261d
mananalisyed23 <manansoh2020@gmail.com>
2026-08-09 18:35:24 +0500

    04-08-26

---
+++ authentication_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:authentication_app/screens/login.dart';
+import 'package:authentication_app/screens/register.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(AuthApp());
+}
+class AuthApp extends StatelessWidget {
+  const AuthApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:RegisterScreen(),
+    );
+  }
+}
---
+++ authentication_app/lib/screens/login.dart
@@ -0,0 +1,32 @@
+import 'package:authentication_app/screens/register.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login Screen'), centerTitle: true),
+      body: ListView(
+        children: [
+          Text('Log In', style: Theme.of(context).textTheme.displayMedium),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => RegisterScreen()),
+              );
+            },
+            child: Text('Register here'),
+          ),
+        ],
+      ),
+    );
+  }
+}
