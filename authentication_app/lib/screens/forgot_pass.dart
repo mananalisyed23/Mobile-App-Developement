801f1a6c6ae7c46ef134ba3b5ddcb8e5f277869d
mananalisyed23 <manansoh2020@gmail.com>
2026-08-13 22:42:46 +0500

    13-08-26

--- authentication_app/lib/screens/forgot_pass.dart
+++ authentication_app/lib/screens/forgot_pass.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ForgotScreen extends StatefulWidget {
@@ -8,21 +9,58 @@ class ForgotScreen extends StatefulWidget {
 }
 
 class _ForgotScreenState extends State<ForgotScreen> {
+  TextEditingController email = TextEditingController();
+  FirebaseAuth auth = FirebaseAuth.instance;
+  bool loading = false;
+
+  void resetpassword() async {
+    setState(() {
+      loading = true;
+    });
+    try {
+      await auth.sendPasswordResetEmail(email: email.text.trim());
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please check your email'),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.toString())));
+    } finally {
+      setState(() {
+        loading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Forgot Password screen')),
-      body: Center(
-        child: ListView(
-          padding: EdgeInsets.all(15),
-          children: [
-            SizedBox(height: 30,),
-            TextFormField(decoration: InputDecoration(hintText: 'Email')),
-            SizedBox(height: 20),
-            ElevatedButton(onPressed: () {}, child: Text('Reset password')),
-          ],
-        ),
-      ),
+      body: loading
+          ? Center(child: CircularProgressIndicator())
+          : Center(
+              child: ListView(
+                padding: EdgeInsets.all(15),
+                children: [
+                  SizedBox(height: 30),
+                  TextFormField(
+                    controller: email,
+                    decoration: InputDecoration(hintText: 'Email'),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      resetpassword();
+                    },
+                    child: Text('Reset password'),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
--- authentication_app/lib/screens/login.dart
+++ authentication_app/lib/screens/login.dart
@@ -3,6 +3,7 @@ import 'package:authentication_app/screens/home_screen.dart';
 import 'package:authentication_app/screens/register.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -27,6 +28,38 @@ class _LoginScreenState extends State<LoginScreen> {
         email: email.text.trim(),
         password: password.text,
       );
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+        (value) => false,
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.toString())));
+    } finally {
+      setState(() {
+        loading = false;
+      });
+    }
+  }
+
+  void continueWithgoogle() async {
+    String webClientid =
+        "1003741581440-huknqbjj2f58gh8t6hpd024ttevvu4aa.apps.googleusercontent.com";
+    try {
+      GoogleSignIn signIn = GoogleSignIn.instance;
+      await signIn.initialize(serverClientId: webClientid);
+      GoogleSignInAccount account = await signIn.authenticate();
+      GoogleSignInAuthentication googleauth = account.authentication;
+      final credential = GoogleAuthProvider.credential(
+        idToken: googleauth.idToken,
+      );
+
+      await auth.signInWithCredential(credential);
+      setState(() {
+        loading = true;
+      });
       Navigator.pushAndRemoveUntil(
         context,
         MaterialPageRoute(builder: (context) => HomeScreen()),
@@ -97,6 +130,12 @@ class _LoginScreenState extends State<LoginScreen> {
                     },
                     child: Text('Forgot password?'),
                   ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Continue with google'),
+                  ),
+                  SizedBox(height: 10),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
