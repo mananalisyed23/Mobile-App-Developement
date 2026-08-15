72eb9d20744ef0bc674f862b7b37eb6325246444
mananalisyed23 <manansoh2020@gmail.com>
2026-08-15 19:25:53 +0500

    14-08-26

---
+++ supabase_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_app/homescreen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://ccchzyonfqowgtydtamo.supabase.co',
+    anonKey: 'sb_publishable_7NimN2dd-zdWqTodBTPVlg_Fx8gze_0',
+  );
+  runApp(SupabaseApp());
+}
+
+class SupabaseApp extends StatelessWidget {
+  const SupabaseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Homescreen());
+  }
+}
